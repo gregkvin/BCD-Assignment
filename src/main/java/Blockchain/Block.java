4b1c4fcff788e978bc6bdf6668b1161501fe0307
gregkvin <tp058475@mail.apu.edu.my>
2023-03-29 12:34:02 +0800

    blockchain

--- src/main/java/Blockchain/Block.java
+++ src/main/java/Blockchain/Block.java
@@ -1,90 +1,90 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package Blockchain;
-//
-//import java.io.Serializable;
-//import Hashing.Hasher;
-//
-///**
-// *
-// * @author putubgs
-// */
-//public class Block implements Serializable{
-//    public Header blockHeader;
-//    public Header getBlockHeader() {
-//        return blockHeader;
-//    }
-//    /* composition relationship */
-//    public Block(int index, String previousHash) {
-//        super();
-//        long now = System.currentTimeMillis();
-//        /* construct part object upon object construction */
-//        this.blockHeader = new Header();
-//        this.blockHeader.setPreviousHash(previousHash);
-//        this.blockHeader.setTimestamp(now);
-//        //hashing with sha256 - the input is joined with previousHash+now
-//        String currentHash = Hasher.sha256( 
-//                String.join("+", previousHash, String.valueOf(now)) );
-//        this.blockHeader.setCurrentHash(currentHash);
-//        System.out.println(Hasher.sha256(String.valueOf(now)));
-//    }
-//    /* composition relationship - inner class definition for part object*/
-//    public class Header implements Serializable{
-//        //data member
-//        private int index;
-//        private String currentHash, previousHash;
-//        private long timestamp;
-//        
-//        @Override
-//        public String toString() {
-//            return "Header [index=" + index + ", currentHash=" + currentHash + ", previousHash=" + previousHash
-//                    + ", timestamp=" + timestamp + "]";
-//        }
-//        
-//        //getset methods
-//        public String getCurrentHash() {
-//            return currentHash;
-//        }
-//        public void setCurrentHash(String currentHash) {
-//            this.currentHash = currentHash;
-//        }
-//        public String getPreviousHash() {
-//            return previousHash;
-//        }
-//        public void setPreviousHash(String previousHash) {
-//            this.previousHash = previousHash;
-//        }
-//        public long getTimestamp() {
-//            return timestamp;
-//        }
-//        public void setTimestamp(long timestamp) {
-//            this.timestamp = timestamp;
-//        }
-//        public void setIndex(int index) {
-//            this.index = index;
-//        }
-//
-//        
-//    }
-//    
-//    /* aggregation relationship */
-//    public Blockchain.Transaction tranxLst;
-//    public void setTranxLst(Transaction tranxLst) {
-//        this.tranxLst = tranxLst;
-//        if (tranxLst != null) {
-//            tranxLst.calculateMerkleRoot();
-//        }
-//    }
-//    @Override
-//    public String toString() {
-//        if (tranxLst != null) {
-//            tranxLst.calculateMerkleRoot();
-//        }
-//        return "Block [blockHeader=" + blockHeader + ", tranxLst=" + tranxLst + "]";
-//    }
-//    
-//    
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Blockchain;
+
+import java.io.Serializable;
+import Hashing.Hasher;
+
+/**
+ *
+ * @author putubgs
+ */
+public class Block implements Serializable{
+    public Header blockHeader;
+    public Header getBlockHeader() {
+        return blockHeader;
+    }
+    /* composition relationship */
+    public Block(int index, String previousHash) {
+        super();
+        long now = System.currentTimeMillis();
+        /* construct part object upon object construction */
+        this.blockHeader = new Header();
+        this.blockHeader.setPreviousHash(previousHash);
+        this.blockHeader.setTimestamp(now);
+        //hashing with sha256 - the input is joined with previousHash+now
+        String currentHash = Hasher.sha256( 
+                String.join("+", previousHash, String.valueOf(now)) );
+        this.blockHeader.setCurrentHash(currentHash);
+        System.out.println(Hasher.sha256(String.valueOf(now)));
+    }
+    /* composition relationship - inner class definition for part object*/
+    public class Header implements Serializable{
+        //data member
+        private int index;
+        private String currentHash, previousHash;
+        private long timestamp;
+        
+        @Override
+        public String toString() {
+            return "Header [index=" + index + ", currentHash=" + currentHash + ", previousHash=" + previousHash
+                    + ", timestamp=" + timestamp + "]";
+        }
+        
+        //getset methods
+        public String getCurrentHash() {
+            return currentHash;
+        }
+        public void setCurrentHash(String currentHash) {
+            this.currentHash = currentHash;
+        }
+        public String getPreviousHash() {
+            return previousHash;
+        }
+        public void setPreviousHash(String previousHash) {
+            this.previousHash = previousHash;
+        }
+        public long getTimestamp() {
+            return timestamp;
+        }
+        public void setTimestamp(long timestamp) {
+            this.timestamp = timestamp;
+        }
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        
+    }
+    
+    /* aggregation relationship */
+    public Transaction tranxLst;
+    public void setTranxLst(Transaction tranxLst) {
+        this.tranxLst = tranxLst;
+        if (tranxLst != null) {
+            tranxLst.calculateMerkleRoot();
+        }
+    }
+    @Override
+    public String toString() {
+        if (tranxLst != null) {
+            tranxLst.calculateMerkleRoot();
+        }
+        return "Block [blockHeader=" + blockHeader + ", tranxLst=" + tranxLst + "]";
+    }
+    
+    
+}
---
+++ src/main/java/Blockchain/BlockLogic.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package Blockchain;
+
+import Class.User;
+import java.io.File;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author putubgs
+ */
+public class BlockLogic {
+
+    public static String masterFolder = "src/main/java/master";
+    public static String userBlock = masterFolder+"/user_information.bin";
+    public static String userFile = masterFolder+"/user_information.txt";
+    public static String certBlock = masterFolder+"/digital_certificate.bin";
+    
+    public void userBlock(User user){
+        Blockchain bc = Blockchain.getInstance( userBlock );
+        Transaction previousTranxLst = bc.get().getLast().tranxLst;
+        Transaction newTranxLst = new Transaction(10);
+        if (previousTranxLst != null) {
+            for (String t : previousTranxLst.getDataLst()) {
+                newTranxLst.add(t);
+            }
+        }
+        String serializedUser = user.toString();
+        System.out.println(serializedUser);
+        newTranxLst.add(serializedUser);
+        System.out.println(newTranxLst + "hehe");
+        String previousHash = bc.get().getLast().getBlockHeader().getCurrentHash();
+        Block b1 = new Block(bc.get().size(), previousHash);
+        b1.setTranxLst(newTranxLst);
+        bc.nextBlock(b1);
+        bc.distribute();
+    }
+    
+    public User findUser(String username, String password, Blockchain bc) {
+        LinkedList<Block> chain = bc.get();
+    
+        for (Block block : chain) {
+            Transaction transactionList = block.tranxLst;
+            if (transactionList != null) {
+                for (String data : transactionList.getDataLst()) {
+                    String[] parts = data.split("\\|");
+                    System.out.println(parts[1]);
+                    if (parts.length >= 5) {
+                        if (parts[1].equals(username) && parts[3].equals(password)) {
+                            User user = new User(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
+                            return user;
+                        }
+                    }
+                }
+            }
+        }
+    
+        return null;
+    }
+    
+    public void certBlock(){
+        Blockchain bc = Blockchain.getInstance( certBlock );
+        Transaction previousTranxLst = bc.get().getLast().tranxLst;
+        Transaction newTranxLst = new Transaction(10);
+        if (previousTranxLst != null) {
+            for (String t : previousTranxLst.getDataLst()) {
+                newTranxLst.add(t);
+            }
+        }
+        String serializedUser = "ya";
+        System.out.println(serializedUser);
+        newTranxLst.add(serializedUser);
+        System.out.println(newTranxLst + "hehe");
+        String previousHash = bc.get().getLast().getBlockHeader().getCurrentHash();
+        Block b1 = new Block(bc.get().size(), previousHash);
+        b1.setTranxLst(newTranxLst);
+        bc.nextBlock(b1);
+        bc.distribute();
+    }
+    
+    public boolean isUserDuplicate(User newUser, Blockchain bc) {
+        LinkedList<Block> chain = bc.get();
+
+        for (Block block : chain) {
+            Transaction transactionList = block.tranxLst;
+            if (transactionList != null) {
+                for (String data : transactionList.getDataLst()) {
+                    String[] parts = data.split("\\|");
+                    if (parts.length >= 5) {
+                        if (parts[1].equals(newUser.getUsername()) || parts[2].equals(newUser.getFullName())) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
